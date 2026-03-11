d23bf0cba8b09d95fd43c206d2b7b7618f18a0bf
ashupal-career <ashuvatsal007@gmail.com>
2026-03-11 22:17:50 +0530

    Second Change

---
+++ react-project-three/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <div>
+      <Card name='Ashu'>
+      <h1>Hello I'm Ashu</h1>
+      </Card>
+    </div>
+  )
+}
+
+export default App
