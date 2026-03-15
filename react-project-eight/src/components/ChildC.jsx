f51864d620595e08d74860418b6140ccb6b29500
ashupal-career <ashuvatsal007@gmail.com>
2026-03-15 22:00:33 +0530

    Fixed error

--- react-project-eight/src/components/ChildC.jsx
+++ react-project-eight/src/components/ChildC.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { UserContext, useContext } from '../App'
+import React, { useContext } from 'react'
+import { UserContext } from '../App'
 
 const ChildC = () => {
     const user = useContext(UserContext);
-  return (
-    <div>
-        {console.log("Hello")}
-        <h1>{user.name}</h1>
-    </div>
-  )
+
+    return (
+        <div>
+            <h1>{user.name}</h1>
+        </div>
+    )
 }
 
 export default ChildC
