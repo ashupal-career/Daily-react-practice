8ea80ce9b6dcbc0236bd9644988dc8d4c2fa2b48
ashupal-career <ashuvatsal007@gmail.com>
2026-03-27 12:46:08 +0530

    Few Update in Submit validation

--- react-form-project/src/App.jsx
+++ react-form-project/src/App.jsx
@@ -7,12 +7,17 @@ function App() {
     register,
     handleSubmit,
     watch,
-    formState: {errors},
+    formState: {errors, isSubmitting},
   } = useForm();
+  async function onSubmit(data) {
+    // API call ko simulator krte h
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("Submitting the form", data);
+  }
 
   return (
     <>
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>First Name: </label>
         <input 
@@ -32,10 +37,17 @@ function App() {
       <br />
       <div>
         <label>Last Name: </label>
-        <input type="text" {...register('lastName')}/>
+        <input type="text" {...register('lastName', {
+            pattern: {
+              value: /^[A-Za-z]+$/i ,
+              message: 'Last name is not as per the rules'
+            }
+          })}/>
+          {errors.lastName && <p className='error-msg'>{errors.lastName.message}</p>}
       </div>
       <br />
-      <input type="submit" />
+      <input type="submit" disabled={isSubmitting}
+      value={isSubmitting ? 'Submitting' : 'Submit'}/>
     </form>
     </>
   )
