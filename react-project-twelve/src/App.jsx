863296834af24ae62215b4d5892dab0de8450b72
ashupal-career <ashuvatsal007@gmail.com>
2026-03-30 21:14:00 +0530

    Frezee function using UseCallBack hook

--- react-project-twelve/src/App.jsx
+++ react-project-twelve/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 import ChildComponent from './components/ChildComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  function handleClick(){
-    setCount(count+1)
-  }
+  const handleClick = useCallback(() => {
+    setCount(count+1);
+  }, [])
   return (
     <>
     Count: {count}
